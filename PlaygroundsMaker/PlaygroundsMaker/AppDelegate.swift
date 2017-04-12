81af36f0f728e7ea3d6db6b88610ea3f9fe6dd4c
Adilson Tavares <adilsonmktavares@gmail.com>
2017-04-12 15:16:22 -0300

    Starter project
    Empty Xcode project.

---
+++ PlaygroundsMaker/PlaygroundsMaker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PlaygroundsMaker
+//
+//  Created by Adilson Tavares on 12/04/17.
+//  Copyright © 2017 Adilson Tavares. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PlaygroundsMaker/PlaygroundsMaker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PlaygroundsMaker
+//
+//  Created by Adilson Tavares on 12/04/17.
+//  Copyright © 2017 Adilson Tavares. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
